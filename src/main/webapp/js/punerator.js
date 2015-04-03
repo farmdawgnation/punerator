f3be4006cc6bd0e26bb595832244e6e45cd40f3a
Matt Farmer <matt@frmr.me>
2015-04-03 16:26:10 -0400

    Wire up some basic KO binding for the puns.

--- src/main/webapp/js/punerator.js
+++ src/main/webapp/js/punerator.js
@@ -1,14 +1,20 @@
 (function($) {
-  var sampleData = ko.observableArray([
-    {
-      id: 'aaa',
-      title: 'Pun 1',
-      content: 'Pun 1 Pun 1 Pun 1'
-    },
-    {
-      id: 'bbb',
-      title: 'Pun 2',
-      content: 'Pun 2 Pun 2 Pun 2'
-    }
-  ]);
+  var punModel = {
+    puns: ko.observableArray([
+      {
+        id: 'aaa',
+        title: 'Pun 1',
+        content: 'Pun 1 Pun 1 Pun 1'
+      },
+      {
+        id: 'bbb',
+        title: 'Pun 2',
+        content: 'Pun 2 Pun 2 Pun 2'
+      }
+    ])
+  };
+
+  $(document).ready(function() {
+    ko.applyBindings(punModel, $(".punstream")[0]);
+  });
 })(jQuery);
