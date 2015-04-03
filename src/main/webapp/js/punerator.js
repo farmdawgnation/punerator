c1d4e131ca890cb561a613c898bc002ed9c30105
Matt Farmer <matt@frmr.me>
2015-04-03 16:21:51 -0400

    Dat JS tho

---
+++ src/main/webapp/js/punerator.js
@@ -0,0 +1,14 @@
+(function($) {
+  var sampleData = ko.observableArray([
+    {
+      id: 'aaa',
+      title: 'Pun 1',
+      content: 'Pun 1 Pun 1 Pun 1'
+    },
+    {
+      id: 'bbb',
+      title: 'Pun 2',
+      content: 'Pun 2 Pun 2 Pun 2'
+    }
+  ]);
+})(jQuery);
