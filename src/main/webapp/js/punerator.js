9f72f46b35cda886be325c0f5913845e7f200992
Matt Farmer <matt@frmr.me>
2015-04-04 15:01:00 -0400

    Start planning for the use of events.

--- src/main/webapp/js/punerator.js
+++ src/main/webapp/js/punerator.js
@@ -1,20 +1,30 @@
 (function($) {
+  var punA = {
+    id: 'aaa',
+    author: 'Antonio',
+    content: 'Pun 1 Pun 1 Pun 1'
+  }
+
+  var punB = {
+    id: 'bbb',
+    author: 'Sid',
+    content: 'Pun 2 Pun 2 Pun 2'
+  }
+
   var punModel = {
-    puns: ko.observableArray([
-      {
-        id: 'aaa',
-        author: 'Antonio',
-        content: 'Pun 1 Pun 1 Pun 1'
-      },
-      {
-        id: 'bbb',
-        author: 'Sid',
-        content: 'Pun 2 Pun 2 Pun 2'
-      }
-    ])
+    puns: ko.observableArray([punA, punB]),
+    punsById: {'aaa': punA, 'bbb', punB},
+    queuedPuns: ko.observableArray([])
   };
 
-  $(document).ready(function() {
-    ko.applyBindings(punModel, $(".punstream")[0]);
-  });
+  $(document)
+    .on('punstream-loaded', function(eventData) {
+      ko.applyBindings(punModel, $(".punstream")[0]);
+    })
+    .on('new-pun', function(eventData) {
+
+    })
+    .on('pun-vote', function(eventData) {
+
+    });
 })(jQuery);
