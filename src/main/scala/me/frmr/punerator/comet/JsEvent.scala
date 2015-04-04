b0b274803c912e26caba8979c62901b7f456f3bb
Matt Farmer <matt@frmr.me>
2015-04-04 15:57:20 -0400

    Give us some sensible object id serialization for the client.

--- src/main/scala/me/frmr/punerator/comet/JsEvent.scala
+++ src/main/scala/me/frmr/punerator/comet/JsEvent.scala
@@ -5,10 +5,24 @@ import net.liftweb.json._
 import net.liftweb.http.js._
   import JE._
 
+import org.bson.types._
+
 class SimpleJsEvent(eventName:String) extends JsCmd {
   import Serialization._
 
-  implicit def typeHints = Serialization.formats(NoTypeHints)
+  implicit def typeHints = Serialization.formats(NoTypeHints) + new Serializer[ObjectId] {
+    val clazz = classOf[ObjectId]
+
+    def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
+      case objectId: ObjectId =>
+        JString(objectId.toString())
+    }
+
+    def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), ObjectId] = {
+      case (TypeInfo(`clazz`, None), JString(objectIdString)) =>
+        new ObjectId(objectIdString)
+    }
+  }
 
   def toJsCmd = {
     Call("jsevent.event", eventName, decompose(this)).cmd.toJsCmd
