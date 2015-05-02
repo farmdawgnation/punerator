a48557449c5f82ff120a6150f7d20668e29c0c6e
Matt Farmer <matt@frmr.me>
2015-05-02 14:55:25 -0400

    Wire up pun submission.

---
+++ src/main/scala/me/frmr/punerator/snippet/PunSubmitter.scala
@@ -0,0 +1,39 @@
+package me.frmr.punerator
+package snippet
+
+import net.liftweb._
+  import http._
+    import js._
+      import JsCmds._
+    import SHtml._
+  import common._
+  import util._
+    import Helpers._
+  import mongodb._
+
+import me.frmr.punerator.model._
+import me.frmr.punerator.actor._
+import me.frmr.punerator.comet._
+
+object PunSubmitter {
+  def persistPun(name: String, pun: String) = {
+    val newPun = Pun(name, pun)
+    newPun.save
+    CometBroadcastActor ! BroadcastCometMessage(
+      NewPunCreated(newPun),
+      "PunstreamComet"
+    )
+
+    Alert("Your pun has been created!")
+  }
+
+  def render = {
+    var name = ""
+    var pun = ""
+
+    makeFormsAjax andThen
+    "#pun-author" #> text(name, name = _) &
+    "#pun-content" #> textarea(pun, pun = _) &
+    "#submit-pun" #> ajaxOnSubmit(() => persistPun(name, pun))
+  }
+}
