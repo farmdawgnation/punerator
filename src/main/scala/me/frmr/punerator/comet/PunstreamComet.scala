4a6a809c74e467335d98381559f5312d1abc07c8
Matt Farmer <matt@frmr.me>
2015-05-02 11:46:06 -0400

    Initial brush of just using comets.

--- src/main/scala/me/frmr/punerator/comet/PunstreamComet.scala
+++ src/main/scala/me/frmr/punerator/comet/PunstreamComet.scala
@@ -14,9 +14,8 @@ import net.liftweb._
 
 import me.frmr.punerator.model._
 
-case class PunstreamLoaded(puns: List[Pun]) extends SimpleJsEvent("punstream-loaded")
-case class NewPun(pun: Pun) extends SimpleJsEvent("new-pun")
-case class PunVote(punId: String, voteType: String) extends SimpleJsEvent("pun-vote")
+case class PunVotedPunny(id: String) extends SimpleJsEvent("pun-voted-punny")
+case class PunVotedTearable(id: String) extends SimpleJsEvent("pun-voted-tearable")
 
 sealed trait PunstreamCometMessage
 case class NewPunCreated(pun: Pun) extends PunstreamCometMessage
@@ -27,19 +26,35 @@ class PunstreamComet extends CometActor {
   implicit val formats = DefaultFormats
   private val initialNumberOfPuns = 20
 
-  var mostRecentPuns = Pun.findAll(Nil, ("createdAt" -> -1), Limit(initialNumberOfPuns))
+  var visiblePuns = Pun.findAll(Nil, ("createdAt" -> -1), Limit(initialNumberOfPuns))
+  var newPuns: List[Pun] = Nil
 
   def render = {
-    S.appendJs(PunstreamLoaded(mostRecentPuns))
-    PassThru
+    ".new-puns" #> (newPuns.isEmpty ? ClearNodes | PassThru) andThen
+    ".new-pun-count *" #> newPuns.length &
+    ".pun" #> visiblePuns.map { pun =>
+      "^ [data-pun-id]" #> pun._id.toString &
+      ".pun-author *" #> pun.author &
+      ".pun-text *" #> pun.pun &
+      ".punny-count *" #> pun.punny &
+      ".tearable-count *" #> pun.tearable &
+      ".vote-punny [onclick]" #> onEvent { _ =>
+        Pun.update("_id" -> pun._id, "$inc" -> ("punny" -> 1))
+        this ! PunVoteCast(pun._id.toString, "punny")
+      } &
+      ".vote-tearable [onclick]" #> onEvent { _ =>
+        Pun.update("_id" -> pun._id, "$inc" -> ("tearable" -> 1))
+        PunVotedTearable(pun._id.toString, "tearable")
+      }
+    }
   }
 
   override def lowPriority = {
     case NewPunCreated(pun: Pun) =>
-      mostRecentPuns = (pun +: mostRecentPuns).take(initialNumberOfPuns)
-      partialUpdate(NewPun(pun))
+      newPuns = pun +: newPuns
+      reRender()
 
     case PunVoteCast(punId, voteType) =>
-      partialUpdate(PunVote(punId, voteType))
+      //TODO
   }
 }
